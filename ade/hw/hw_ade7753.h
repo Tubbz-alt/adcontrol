b2016a853b3c1a030e787b80fc4ce6f32b532d6e
Patrick Bellasi <derkling@gmail.com>
2011-04-01 20:50:45 +0200

    Bugfixed ADE HW code to handle CS
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

--- ade/hw/hw_ade7753.h
+++ ade/hw/hw_ade7753.h
@@ -64,10 +64,12 @@
 #define ADE7753_CS_HIGH()        do {\
 	PORTB |= _BV(PB4);\
 	PORTC &= ~_BV(PC2);\
+	PORTA |= (_BV(PA4) | _BV(PA5));\
 } while(0)
 #define ADE7753_CS_LOW()         do {\
 	PORTB &= ~_BV(PB4);\
 	PORTC |= _BV(PC2);\
+	PORTA &= ~(_BV(PA4) | _BV(PA5));\
 } while(0)
 /*@}*/
 
@@ -77,6 +79,8 @@
 INLINE void meter_ade7753_hw_bus_init(void)
 {
 	/* Implement me! */
+	DDRA |= (_BV(PA4) | _BV(PA5));
+	ADE7753_CS_HIGH();
 }
 
 #endif /* HW_ADE7753_H */
