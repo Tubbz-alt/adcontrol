d36a540a3ef7fa15274e5e0531b92c1072be6786
Patrick Bellasi <derkling@gmail.com>
2011-07-18 23:15:57 +0200

    [Signals] Add GSM RI support
    This patch add support for GSM RI signal. This required to support two
    different ports: PORTC and the newly introduced PORTD.
    This support has been added using a vectored access to port related signals.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

--- ade/signals.c
+++ ade/signals.c
@@ -38,40 +38,64 @@ void intr_rtc_irq(uint8_t level);
 void intr_unit_irq(uint8_t level);
 void intr_plat_button(uint8_t level);
 void intr_plat_i2c(uint8_t level);
+void intr_gsm_ri(uint8_t level);
 
 // The mask of enabled Interrupt pins on PORTC
-static const uint8_t portc_mask = 0xFC;
+static const uint8_t port_mask[SIGNAL_PORT_COUNT] = {0xFC, 0x80};
 // The mask of pulled-up Interrupt pins on PORTC
-static const uint8_t portc_pullups = 0x7C;
+static const uint8_t port_pullups[SIGNAL_PORT_COUNT] = {0x7C, 0x80};
 // The mask of (default) enabled Interrupt pins on PORTC
-static const uint8_t signals_enabled = 0x3C;
-// The bit values on PORTC
-uint8_t signals_status = 0x00;
+static const uint8_t port_signals_enabled[SIGNAL_PORT_COUNT] = {0x3C, 0x80};
+// The PIN change mask
+volatile unsigned char * PROGMEM port_PCMSK[SIGNAL_PORT_COUNT] =
+	{&PCMSK2, &PCMSK3}; 
+
+// The bit values
+uint8_t signals_status[SIGNAL_PORT_COUNT] = {0x00, 0x00};
 // The mask of signals which are pedning (waiting to be processed)
-uint8_t signals_pending = 0x00;
+uint8_t signals_pending[SIGNAL_PORT_COUNT] = {0x00, 0x00};
 
-#define PORTC_READ (portc_mask & PINC)
+#define PORTC_READ (port_mask[SIGNAL_PORTC] & PINC)
+#define PORTD_READ (port_mask[SIGNAL_PORTD] & PIND)
 
-#define SET_SIGNAL(NUM)\
-	(signals_pending |= BV8(SIGNAL_##NUM))
+void set_signal(uint8_t sig);
+void set_signal(uint8_t sig) {
+	uint8_t port = 0;
 
-#define SET_AND_DISABLE(NUM)\
-	signals_pending |= BV8(SIGNAL_##NUM);\
-	PCMSK2 &= ~BV8(SIGNAL_##NUM)
+	while (!(sig & 0x07)) {
+		port++;
+		sig >>= 3;
+	}
 
+	if (port >= SIGNAL_PORT_COUNT)
+		return;
+
+	signals_pending[port] |= BV8(sig);
+}
+
+void set_and_disable(uint8_t sig);
+void set_and_disable(uint8_t sig) {
+	uint8_t port = 0;
+
+	while (!(sig & 0x07)) {
+		port++;
+		sig >>= 3;
+	}
+
+	if (port >= SIGNAL_PORT_COUNT)
+		return;
+
+	signals_pending[port] |= BV8(sig);
+	*port_PCMSK[port] &= ~BV8(sig);
+}
 
 //----- Signals utility functions
 
 // Wait for the specified signal
 void signal_wait(uint8_t sig) {
 	signal_enable(sig);
-//kprintf("\nSingnals: e(0x%02X), s(0x%02X)\n", PCMSK2, signals_pending);
 	while (!signal_pending(sig)) {
 		cpu_relax();
-//		DB(
-//				timer_delay(100);
-//				kprintf("v");
-//		)
 	}
 }
 
@@ -79,53 +103,63 @@ void signal_wait(uint8_t sig) {
 void signals_init(void) {
 
 	// Enable required pull-ups
-	PORTC |=  portc_pullups;
+	PORTC |=  port_pullups[SIGNAL_PORTC];
 	// Configure PINs as INPUT
-	DDRC  &= ~portc_mask;
+	DDRC  &= ~port_mask[SIGNAL_PORTC];
 	// Enable required interrupts PCINT[18..19]
-	PCMSK2 = signals_enabled;
+	PCMSK2 = port_signals_enabled[SIGNAL_PORTC];
 	// Getting current status of PORTC
-	signals_status = PORTC_READ;
-	// Enable interrupt on PORTC
-	PCICR |= BV8(PCIE2);
+	signals_status[SIGNAL_PORTC] = PORTC_READ;
+
+	// Enable required pull-ups
+	PORTD |=  port_pullups[SIGNAL_PORTD];
+	// Configure PINs as INPUT
+	DDRD  &= ~port_mask[SIGNAL_PORTD];
+	// Enable required interrupts PCINT[18..19]
+	PCMSK3 = port_signals_enabled[SIGNAL_PORTD];
+	// Getting current status of PORTC
+	signals_status[SIGNAL_PORTD] = PORTD_READ;
+
+	// Enable interrupt on PORTC and PORTD
+	PCICR |= (BV8(PCIE2) | BV8(PCIE3));
 
 }
 
 //----- Interrupt handlers
 void intr_ade_zx(uint8_t level) {
-//PINA = 0x80;
 	(void)level;
-	SET_AND_DISABLE(ADE_ZX);
+	set_and_disable(SIGNAL_ADE_ZX);
 }
 
 void intr_ade_irq(uint8_t level) {
 	(void)level;
-	SET_AND_DISABLE(ADE_IRQ);
+	set_and_disable(SIGNAL_ADE_IRQ);
 }
 
 void intr_rtc_irq(uint8_t level) {
 	(void)level;
-	SET_AND_DISABLE(RTC_IRQ);
+	set_and_disable(SIGNAL_RTC_IRQ);
 }
 
 void intr_unit_irq(uint8_t level) {
-//PINA = 0x40;
 	(void)level;
-	SET_AND_DISABLE(UNIT_IRQ);
+	set_and_disable(SIGNAL_UNIT_IRQ);
 }
 
 void intr_plat_button(uint8_t level) {
-//PINA = 0x20;
 	(void)level;
-	SET_SIGNAL(PLAT_BUTTON);
+	set_signal(SIGNAL_PLAT_BUTTON);
 }
 
 void intr_plat_i2c(uint8_t level) {
-//PINA = 0x10;
 	(void)level;
-	SET_SIGNAL(PLAT_I2C);
+	set_signal(SIGNAL_PLAT_I2C);
 }
 
+void intr_gsm_ri(uint8_t level) {
+	(void)level;
+	set_and_disable(SIGNAL_GSM_RI);
+}
 
 #if CONFIG_SIG_TESTING
 # warning SIGNALS TESTING ENABLED
@@ -133,11 +167,11 @@ void sigTesting() {
 
 	LOG_INFO(".:: External Interrupt Testing\r\n");
 
-	while(1) {
+	while (1) {
 
 		timer_delay(1000);
 
-		LOG_INFO("PINC: 0x%02X\r\n", PINC);
+		LOG_INFO("PINC: 0x%02X, PIND: 0x%02X\r\n", PINC, PIND);
 		if (signal_pending(SIGNAL_PLAT_BUTTON)) {
 			LOG_INFO("EVT: Button [%hd]\r\n",
 					signal_status(SIGNAL_PLAT_BUTTON));
@@ -145,6 +179,10 @@ void sigTesting() {
 		if (signal_pending(SIGNAL_UNIT_IRQ)) {
 			LOG_INFO("EVT: RCT Unit Fault [%hd]\r\n",
 					signal_status(SIGNAL_UNIT_IRQ));
+		}
+		if (signal_pending(SIGNAL_GSM_RI)) {
+			LOG_INFO("EVT: GSM RI [%hd]\r\n",
+					signal_status(SIGNAL_GSM_RI));
 		}
 	}
 }
@@ -154,12 +192,15 @@ typedef void (*intrHandler_t)(uint8_t level);
 
 // Interrupt handlers on PORTC
 intrHandler_t intrTable[] = {
+	//--- PORTC
 	intr_plat_i2c, 			// PC2 - I2C Interrupt
 	intr_plat_button,		// PC3 - User Button
 	intr_unit_irq,			// PC4 - RCT Unit Fault
 	intr_rtc_irq,			// PC5 - RTC Interrupt
 	intr_ade_irq,			// PC6 - ADE Interrupt
-	intr_ade_zx				// PC7 - ADE Zero Crossing
+	intr_ade_zx,			// PC7 - ADE Zero Crossing
+	//--- PORTD
+	intr_gsm_ri				// PD7 - GSM Ring
 };
 
 //----- Low-level Interrupt handler
@@ -168,7 +209,7 @@ DECLARE_ISR_CONTEXT_SWITCH(PCINT2_vect) {
 	uint8_t portc_changed;
 
 	// Getting changes bits
-	portc_changed = (signals_status ^ portc_levels);
+	portc_changed = (signals_status[SIGNAL_PORTC] ^ portc_levels);
 
 	// Calling handler of changed signals
 	for (uint8_t sig = 0x04, i = 0; sig; sig<<=1, i++) {
@@ -176,7 +217,23 @@ DECLARE_ISR_CONTEXT_SWITCH(PCINT2_vect) {
 			(*intrTable[i])(portc_levels);
 	}
 
-	signals_status = portc_levels;
+	signals_status[SIGNAL_PORTC] = portc_levels;
 
 }
 
+DECLARE_ISR_CONTEXT_SWITCH(PCINT3_vect) {
+	uint8_t portd_levels = PORTD_READ;
+	uint8_t portd_changed;
+
+	// Getting changes bits
+	portd_changed = (signals_status[SIGNAL_PORTD] ^ portd_levels);
+
+	// Calling handler of changed signals
+	for (uint8_t sig = 0x80, i = 6; sig; sig<<=1, i++) {
+		if (portd_changed & sig)
+			(*intrTable[i])(portd_levels);
+	}
+
+	signals_status[SIGNAL_PORTD] = portd_levels;
+
+}
--- ade/signals.h
+++ ade/signals.h
@@ -25,49 +25,125 @@
 #include <cfg/cfg_sig.h>
 #include <avr/io.h>
 
-#define SIGNAL_ADE_ZX 		7
-#define SIGNAL_ADE_IRQ  	6
-#define SIGNAL_RTC_IRQ		5
-#define SIGNAL_UNIT_IRQ		4
-#define SIGNAL_PLAT_BUTTON 	3
-#define SIGNAL_PLAT_I2C		2
+// Signals on PORTD [8..15]
+#define SIGNAL_GSM_RI		56
+// Signals on PORTC [0..7]
+#define SIGNAL_ADE_ZX 		 7
+#define SIGNAL_ADE_IRQ  	 6
+#define SIGNAL_RTC_IRQ		 5
+#define SIGNAL_UNIT_IRQ		 4
+#define SIGNAL_PLAT_BUTTON 	 3
+#define SIGNAL_PLAT_I2C		 2
 
-// The signal flag for each pin on PORTC
-extern uint8_t signals_pending;
-extern uint8_t signals_status;
+#define SIGNAL_PORTC 		 0
+#define SIGNAL_PORTD 		 1
+#define SIGNAL_PORT_COUNT    2
+
+// The signal flag for each pin on the respective port
+extern uint8_t signals_pending[SIGNAL_PORT_COUNT];
+extern uint8_t signals_status[SIGNAL_PORT_COUNT];
+extern volatile unsigned char *port_PCMSK[SIGNAL_PORT_COUNT];
 
 // Verify if a signal has been set
 inline uint8_t signal_pending(uint8_t sig);
 inline uint8_t signal_pending(uint8_t sig) {
-	uint8_t result = (signals_pending & BV8(sig));
+	uint8_t port = 0;
+	uint8_t result;
+
+	while (!(sig & 0x07)) {
+		port++;
+		sig >>= 3;
+	}
+
+	if (port >= SIGNAL_PORT_COUNT)
+		return 0;
+
+	result = (signals_pending[port] & BV8(sig));
 	if (result) {
-		signals_pending &= ~BV8(sig);
+		signals_pending[port] &= ~BV8(sig);
 		return 1;
 	}
+
 	return 0;
 }
 
 // Get the value of the PIN
 inline uint8_t signal_status(uint8_t sig);
 inline uint8_t signal_status(uint8_t sig) {
-	if (signals_status & BV8(sig))
+	uint8_t port = 0;
+
+	while (!(sig & 0x07)) {
+		port++;
+		sig >>= 3;
+	}
+
+	if (port >= SIGNAL_PORT_COUNT)
+		return 0;
+
+	if (signals_status[port] & BV8(sig))
 		return 1;
+
 	return 0;
 }
 
 // Enable the specified signal
 inline void signal_enable(uint8_t sig);
 inline void signal_enable(uint8_t sig) {
-	// Reset signal flag
-	signals_pending &= ~BV8(sig);
-	// Enable signal interrupt
-	PCMSK2 |= BV8(sig);
+	uint8_t port = 0;
+
+	while (!(sig & 0x07)) {
+		port++;
+		sig >>= 3;
+	}
+
+	if (port >= SIGNAL_PORT_COUNT)
+		return;
+
+	signals_pending[port] &= ~BV8(sig);
+	*port_PCMSK[port] |= BV8(sig);
+
+#if 0
+	if (sig & 0x07) {
+		// Reset signal flag
+		signals_pending[SIGNAL_PORTC] &= ~BV8(sig);
+		// Enable signal interrupt
+		PCMSK2 |= BV8(sig);
+	}
+	sig >>= 3;
+	if (sig & 0x07) {
+		// Reset signal flag
+		signals_pending[SIGNAL_PORTD] &= ~BV8(sig);
+		// Enable signal interrupt
+		PCMSK3 |= BV8(sig);
+	}
+#endif
 }
 
 // Disable the specified signal
 inline void signal_disable(uint8_t sig);
 inline void signal_disable(uint8_t sig) {
-	PCMSK2 &= ~BV8(sig);
+	uint8_t port = 0;
+
+	while (!(sig & 0x07)) {
+		port++;
+		sig >>= 3;
+	}
+
+	if (port >= SIGNAL_PORT_COUNT)
+		return;
+
+	*port_PCMSK[port] &= ~BV8(sig);
+
+#if 0
+	if (sig & 0x07) {
+		PCMSK2 &= ~BV8(sig);
+		return;
+	}
+	sig >>= 3;
+	if (sig & 0x07) {
+		PCMSK3 &= ~BV8(sig);
+	}
+#endif
 }
 
 #if CONFIG_SIG_TESTING
