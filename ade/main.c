00bddb5e41e122424a2dbf760731ce01caff03a3
Patrick Bellasi <derkling@gmail.com>
2011-09-23 14:50:52 +0200

    [Main] Add STACK checking
    This patch add a simple STACK painter and checker which is run after each
    control loop call. If a stack overflow is detected: a reset is forced.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

--- ade/main.c
+++ ade/main.c
@@ -70,6 +70,82 @@ static void notifyPowerOn(void) {
 	//notifyAllBySMS(msg);
 }
 
+//=====[ Stack Usage Monitoring ]===============================================
+extern uint8_t _end; 
+extern uint8_t __stack;
+#define STACK_CANARY 0xC5
+
+void StackPaint(void)
+	__attribute__ ((naked))
+	__attribute__ ((section (".init1")));
+void StackPaint(void) { 
+#if 0 
+    uint8_t *p = &_end; 
+
+    while(p <= &__stack) 
+    { 
+        *p = STACK_CANARY; 
+        p++; 
+    } 
+#else
+	__asm volatile (""
+"    ldi r30,lo8(_end)\n" 
+"    ldi r31,hi8(_end)\n" 
+"    ldi r24,lo8(0xc5)\n" /* STACK_CANARY = 0xc5 */ 
+"    ldi r25,hi8(__stack)\n" 
+"    rjmp .cmp\n" 
+".loop:\n" 
+"    st Z+,r24\n" 
+".cmp:\n" 
+"    cpi r30,lo8(__stack)\n" 
+"    cpc r31,r25\n" 
+"    brlo .loop\n" 
+"    breq .loop"::); 
+#endif 
+}
+
+// Get a count of available STACK memory
+uint16_t StackCount(void);
+uint16_t StackCount(void)  { 
+    const uint8_t *p = &_end; 
+    uint16_t       c = 0; 
+    while (*p == STACK_CANARY &&
+			p <= &__stack) { 
+        p++; c++; 
+    } 
+    return c; 
+}
+
+// Keep track of MIN stach size
+void CheckStack(void);
+void CheckStack(void) {
+	static uint16_t maxStack = 0x0000;
+	uint16_t freeStack = 0;
+
+	// Check for available STACK size
+	freeStack = StackCount();
+
+	// Update MAX free stack sizes
+	if (maxStack < freeStack) {
+		maxStack = freeStack;
+	}
+
+	//kprintf("Free stack (min/max): %d/%d\n", freeStack, maxStack);
+
+	if (freeStack != 0)
+		return;
+
+	// Stack overflow has occurred
+	kprintf("\n\n\n!!!!! Stack overflow !!!!!\nResetting...\n\n\n");
+	wdt_enable(WDTO_1S);
+	while(1);
+	
+}
+
+
+
+//=====[ Initialization and Main ]==============================================
+
 static void init(void) {
 
 	getResetReason();
@@ -95,6 +171,8 @@ int main(void) {
 
 	init();
 	LED_ON();
+
+	CheckStack();
 
 	kprintf("RFN (c) 2011 RCT\r\nBuildNr %d\r\n", vers_build_nr);
 	printResetReason();
@@ -156,6 +234,7 @@ int main(void) {
 
 	/* Main control loop */
 	while(1) {
+		CheckStack();
 		controlLoop();
 	}
 
